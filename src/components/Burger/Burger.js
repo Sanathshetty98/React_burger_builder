28ebc69c756890a20c34669a8d782b21887b2ba4
Sanathshetty98 <shetty.sanath98@gmail.com>
2019-10-01 16:14:04 +0530

    1-Oct-2019

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
-import { arrayExpression } from '@babel/types';
 
 const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
