1b3021f7b19064e33c66821f7c6021f907b53149
Sanathshetty98 <shetty.sanath98@gmail.com>
2019-10-09 11:48:26 +0530

    9-Oct-2019

---
+++ src/hoc/withErrorHandling/withErrorHandling.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Auxiliary';
+
+const withErrorHandler = ( WrapperComponent,axios ) => {
+    return class extends Component {
+        state = {
+            error: null
+        }
+        componentDidMount() {
+            axios.interceptors.request.use( req => {
+                this.setState( { error: null} );
+                return req;
+
+            });
+            axios.interceptors.response.use( res => res, error => {
+                this.setState( { error: error} );
+
+            });
+        }
+        errorHandler = () =>{
+            this.setState({error: null});
+        }
+        render() {
+        return (
+            <Aux>
+                <Modal 
+                show={this.state.error}
+                modalClosed={this.errorHandler}>
+                    { this.state.error ? this.state.error.message : null}
+                </Modal>
+                <WrapperComponent {...this.props} />
+            </Aux>
+        );
+        }
+    }
+}
+
+export default withErrorHandler;
