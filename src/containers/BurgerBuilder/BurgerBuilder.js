2f9101f797b48d3cdfc872a47588bdb9c7bdde2e
Sanathshetty98 <shetty.sanath98@gmail.com>
2019-10-11 10:58:00 +0530

    11-Oct-2019

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React,{ Component} from 'react';
+
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -105,7 +106,15 @@ class BurgerBuilder extends Component{
           .catch(error => 
             this.setState( { loading: false, purchasing: false} ));
             */
-           this.props.history.push('/checkout');
+           const queryParams = [];
+            for(let i in this.state.ingredients){
+                queryParams.push( encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+            }
+           const queryString = queryParams.join('&');
+           this.props.history.push({
+            pathname:  '/checkout',
+            search: '?' + queryString
+           });
     }
     render() {
         const disabledInfo = {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React,{ Component } from 'react';
 import CheckOutSummary from '../../components/Order/CheckOutSummary/CheckOutSummary'
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component{
     state = {
@@ -9,6 +11,14 @@ class Checkout extends Component{
             cheese: 1,
             meat: 1
         }
+    }
+    componentDidMount(){
+        const query = new URLSearchParams(this.props.location.search)
+        const ingredients = {};
+        for(let param of query.entries()){
+            ingredients[param[0]] = +param[1]
+        }
+        this.setState( { ingredients:ingredients } );
     }
     checkoutCancel=()=>{
         this.props.history.goBack();
@@ -23,6 +33,7 @@ class Checkout extends Component{
                     ingredients={this.state.ingredients} 
                     checkoutCancel={this.checkoutCancel}
                     checkoutContinue={this.checkoutContinue}/>
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
             </div>
         );
     }
